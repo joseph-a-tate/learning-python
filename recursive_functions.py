8d6e44b1c951093f8765f61306705bb6caba0d05
Utgarda-loki18 <joseph-a-tate@proton.me>
2025-02-08 18:24:59 +0900

    added factorial function

--- recursive_functions.py
+++ recursive_functions.py
@@ -3,5 +3,12 @@ def func(message, counter):
         func(message, counter-1)
     print(message + " " + str(counter))
 
+def factorial(num):
+    value = num
+    if num > 1:
+        value *= factorial(num-1)
+    return value
+
 if __name__ == "__main__":
     func("Hello bob", 5)
+    print("Factorial 6: " + str(factorial(6)))
